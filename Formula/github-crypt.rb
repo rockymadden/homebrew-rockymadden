b9ce13520a743ac9ab2b04c8bfa9ee2fbf136d39
Rocky Madden <git@rockymadden.com>
2015-11-01 17:49:42 -0700

    github-crypt v0.1.0

--- Formula/github-crypt.rb
+++ Formula/github-crypt.rb
@@ -1,9 +1,9 @@
 class GithubCrypt < Formula
   homepage "https://github.com/rockymadden/github-crypt"
-  url "https://github.com/rockymadden/github-crypt/archive/v0.0.1.tar.gz"
-  sha1 "3fe7aa78064d71fa496d835a93455cf11f0cb26f"
+  url "https://github.com/rockymadden/github-crypt/archive/v0.1.0.tar.gz"
+  sha1 "d4d3dc7207a5b8a6d5682b9ebb7c7b0f6a996ccb"
 
   def install
-    bin.install "src/ghcrypt"
+    bin.install "src/github-crypt"
   end
 end
