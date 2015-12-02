ad22ef198050dd740e714a61b31e207b243dd8ff
Rocky Madden <git@rockymadden.com>
2015-12-01 19:51:56 -0700

    slack-cli: Fixed formula name

--- Formula/slack-cli.rb
+++ Formula/slack-cli.rb
@@ -1,4 +1,4 @@
-class GithubCrypt < Formula
+class SlackCli < Formula
   homepage "https://github.com/rockymadden/slack-cli"
   url "https://github.com/rockymadden/slack-cli/archive/v0.0.0.tar.gz"
   sha256 "aabb599c955eeff8d1137f744ab42e6b778d69e5ea5c998c363e2b7955b98f7"
