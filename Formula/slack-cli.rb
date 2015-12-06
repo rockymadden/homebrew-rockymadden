ce8f3073b67cc60a4625183a20d48d83031ae552
Rocky Madden <git@rockymadden.com>
2015-12-05 20:35:11 -0700

    slack-cli: v0.2.0

--- Formula/slack-cli.rb
+++ Formula/slack-cli.rb
@@ -1,7 +1,7 @@
 class SlackCli < Formula
   homepage "https://github.com/rockymadden/slack-cli"
-  url "https://github.com/rockymadden/slack-cli/archive/v0.1.0.tar.gz"
-  sha256 "cca65e6e467f292b768428b9d856464c31ec45122f05301475911d1c5ce4360c"
+  url "https://github.com/rockymadden/slack-cli/archive/v0.2.0.tar.gz"
+  sha256 "ecabd9adc80fe012e6d8c3a52665bbb0c1ffd2bbfd7224ae5e3f51e965d00e43"
 
   def install
     bin.install "src/slack"
