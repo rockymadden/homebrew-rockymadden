e5bca19fc23bfecb20eff7fe96d26e5cdcce2fcf
Rocky Madden <git@rockymadden.com>
2015-12-23 09:41:22 -0700

    SHA256 used across the board

--- Formula/github-crypt.rb
+++ Formula/github-crypt.rb
@@ -3,12 +3,12 @@ class GithubCrypt < Formula
   url "https://github.com/rockymadden/github-crypt/archive/v0.4.0.tar.gz"
   sha256 "56152739713aa43119e99d38cd7b1a8a407c526e7349f69f667166903b9b1792"
 
+  depends_on "coreutils"
+
   def install
     bin.mkpath
     system "make", "install", "PREFIX=#{prefix}"
   end
-
-  depends_on "coreutils"
 
   test do
     system "github-crypt", "--help"
--- Formula/jsonparse.rb
+++ Formula/jsonparse.rb
@@ -1,7 +1,7 @@
 class Jsonparse < Formula
   homepage "https://github.com/rockymadden/JSON.sh"
   url "https://github.com/rockymadden/JSON.sh/archive/v0.2.1.tar.gz"
-  sha1 "8520d3ddb211f314d24397cd6eb2e9b9e2d5d259"
+  sha256 "1fa727f8bc877d4aee4c8b89fa5d1a72a19104da62cf570d7ad95245823c821f"
 
   def install
     bin.install "JSON.sh" => "jsonparse"
