7752a475cb8d16e8c1c808b44b7faee4113f6738
Rocky Madden <git@rockymadden.com>
2016-01-17 15:13:43 -0700

    circleci-cli: v0.3.0

--- Formula/circleci-cli.rb
+++ Formula/circleci-cli.rb
@@ -1,7 +1,7 @@
 class CircleciCli < Formula
   homepage "https://github.com/rockymadden/circleci-cli"
-  url "https://github.com/rockymadden/circleci-cli/archive/v0.2.0.tar.gz"
-  sha256 "3d908a7ab8ab50d8285d2d417b14a3345305170408a2f6970a7f8b3364140c38"
+  url "https://github.com/rockymadden/circleci-cli/archive/v0.3.0.tar.gz"
+  sha256 "98a4a31d13c834b6d1c4ca59eed385c140079cd331616e1b5d1a9af5e6557672"
 
   depends_on "coreutils"
   depends_on "hub"
