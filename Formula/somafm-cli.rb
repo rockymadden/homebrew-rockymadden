e588dbe216d8468735696c53925188bbac9930be
Rocky Madden <git@rockymadden.com>
2018-05-28 20:45:46 -0600

    somafm-cli: v0.3.1

--- Formula/somafm-cli.rb
+++ Formula/somafm-cli.rb
@@ -1,7 +1,7 @@
 class SomafmCli < Formula
   homepage "https://github.com/rockymadden/somafm-cli"
-  url "https://github.com/rockymadden/somafm-cli/archive/v0.3.0.tar.gz"
-  sha256 "1d48dfbb62feb98fa38c8b3f61b875ff8dfba13c7cc49d4e077f606afce1d110"
+  url "https://github.com/rockymadden/somafm-cli/archive/v0.3.1.tar.gz"
+  sha256 "6b8a18549cf7db11ac50b86d8d792b521f1936fe39b1fbcbbf12c0f6d030e71c"
 
   depends_on "coreutils"
   depends_on "jq"
